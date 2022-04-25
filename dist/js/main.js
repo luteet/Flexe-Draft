1015d1d038c1956c0d597fdff810c1e4781e6936
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2022-04-25 20:00:41 +0300

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,81 @@
+
+const body = document.querySelector('body'),
+    html = document.querySelector('html'),
+    menu = document.querySelectorAll('._burger, .header__nav, body'),
+    burger = document.querySelector('._burger'),
+    header = document.querySelector('.header');
+
+
+
+let thisTarget;
+body.addEventListener('click', function (event) {
+
+    thisTarget = event.target;
+
+    // Меню в шапке
+    if (thisTarget.closest('._burger')) {
+        menu.forEach(elem => {
+            elem.classList.toggle('_active')
+        })
+    }
+
+
+
+    let dropDownBtn = thisTarget.closest('._drop-down-btn');
+    if(dropDownBtn) {
+
+      dropDownBtn.classList.add('_active');
+
+    } else if(!thisTarget.closest('._drop-down')) {
+      document.querySelectorAll('._drop-down-btn').forEach(thisDropDownBtn => {
+        thisDropDownBtn.classList.remove('_active');
+      })
+    }
+
+})
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=- <slider> -=-=-=-=-=-=-=-=-=-=-=-=
+/*
+let slider = new Swiper('.__slider', {
+  
+    spaceBetween: 30,
+    slidesPerView: 1,
+    centeredSlides: false,
+
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      992: {
+        slidesPerView: 3,
+        centeredSlides: true,
+    
+      },
+      600: {
+        slidesPerView: 2,
+        centeredSlides: false,
+      },
+    }
+}); 
+*/
+// =-=-=-=-=-=-=-=-=-=-=-=- </slider> -=-=-=-=-=-=-=-=-=-=-=-=
+
+
+/* 
+// =-=-=-=-=-=-=-=-=-=-=-=- <Анимации> -=-=-=-=-=-=-=-=-=-=-=-=
+
+wow = new WOW({
+mobile:       false,
+})
+wow.init();
+
+// =-=-=-=-=-=-=-=-=-=-=-=- </Анимации> -=-=-=-=-=-=-=-=-=-=-=-=
+
+*/
